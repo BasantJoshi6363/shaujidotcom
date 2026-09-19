f55d2896ae59bc411151143fc2354635c3536635
BasantJoshi6363 <basantjoshi6363@gmail.com>
2026-09-19 15:28:38 +0545

    feat: refactor profile view to use layout component

--- resources/views/protect/profile.blade.php
+++ resources/views/protect/profile.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    hello from profile
-</body>
-</html>
+<x-layout>
+    <h1>hello from profile</h1>
+</x-layout>
